0ba7204973ead20f985a372b63cc5f1ccf82dbda
Mari test <missuri82@gmail.com>
2023-03-25 15:43:09 +0300

    modified added

--- src/main/java/ru/netology/Homework5_1/Services/RestingService.java
+++ src/main/java/ru/netology/Homework5_1/Services/RestingService.java
@@ -1,17 +1,4 @@
-package ru.netology.Homework5_1.Services;
+package ru.neto
 
 public class RestingService {
-    public int calculate(int income, int expenses, int threshold) {
-        int count = 0; // счётчик месяцев отдыха
-        int money = 0; // количество денег на счету
-        for (int month = 0; month < 12; month++) {
-            if (money >= threshold) { // можем ли отдыхать?
-                count++; // увеличиваем счётчик месяцев отдыха
-                money = (money - expenses) / 3; // расчет денег на счету
-            } else {
-                money = money + income - expenses;
-            }
-        }
-        return count;
-    }
 }
